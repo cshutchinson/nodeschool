b1c168d2af5746fafa71ee27f906d92dccdd8552
Christopher Hutchinson <cshutchinson@gmail.com>
2015-09-07 18:59:22 -0600

    remove comments

--- program4.js
+++ program4.js
@@ -2,9 +2,6 @@
 
 	let arg1 = process.argv[2];
     let arg2 = process.argv[3];
-    // let PI = require('./Math').PI;
-    // let sqrt = require('./Math').sqrt;
-    // let square = require('./Math').square;
     
     console.log(PI);
     console.log(sqrt(+arg1));
